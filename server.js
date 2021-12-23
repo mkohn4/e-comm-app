14bb53348ce646c5127bdb6ee350328ce5ed0267
maxkohn <kohn.max@gmail.com>
2021-12-23 15:34:49 -0500

    updated server js to remove comments

--- server.js
+++ server.js
@@ -17,17 +17,3 @@ sequelize.sync({force: false}).then( ()  => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-/*
-Video Acceptance Criteria:
-* The walkthrough video must demonstrate how to create the schema from the MySQL shell.
- - mysql -u root -p source db/schema.sql;
- - USE ecommerce_db;
- - quit;
-* The walkthrough video must demonstrate how to seed the database from the command line.
-- npm run seed
-* The walkthrough video must demonstrate how to start the application’s server.
-- npm start
-* The walkthrough video must demonstrate GET routes for all categories, all products, and all tags being tested in Insomnia Core.
-* The walkthrough video must demonstrate GET routes for a single category, a single product, and a single tag being tested in Insomnia Core.
-* The walkthrough video must demonstrate POST, PUT, and DELETE routes for categories, products, and tags being tested in Insomnia Core.
-*/
